81aaaa74724b2782351a27bf7b4be3314ceaa2a7
Pewaukee <misterkar6@gmail.com>
2023-06-12 13:58:49 -0700

    setup changes/configuration

---
+++ app/footer.tsx
@@ -0,0 +1,13 @@
+// create the footer component
+import Link from "next/link";
+import styles from "./navbar.module.css";
+
+const Footer: React.FC = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Footer;
---
+++ app/navbar.tsx
@@ -0,0 +1,13 @@
+// create the navbar component
+import Link from "next/link";
+import styles from "./navbar.module.css";
+
+const Navbar: React.FC = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Navbar;
