0c3f149223d0cf659661162eb071217f45a7b30c
Pewaukee <misterkar6@gmail.com>
2023-07-05 14:56:57 -0700

    side by side blog

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,23 +2,37 @@ import { getPostBySlug } from "../../../lib/api";
 import markdownToHtml from "../../../lib/markdownToHtml";
 import markdownStyles from "./markdown-styles.module.css";
 import DateFormatter from "../../../components/DateFormatter";
+import Image from "next/image";
 
 export default async function Post({ params }: { params: { slug: string } }) {
-  const post = getPostBySlug(params.slug, ["title", "author", "content", "date"]);
+  const post = getPostBySlug(params.slug, ["title", "author", "content", "date", "coverImage"]);
 
   const content = await markdownToHtml(post.content || "");
 
   return (
     <div className="container mx-auto">
       <main className="m-10">
-        <div className="w-full h-auto text-white">
-          <p className="text-2xl">{post.title}</p>
-          <DateFormatter dateString={post.date} />
-          <div
-            className={markdownStyles["markdown"]}
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
+        <div className="grid grid-cols-2 gap-6">
+          <div className="sticky top-0">
+            <Image
+              src={post.coverImage}
+              alt={`cover image for ${post.title}`}
+              width={400}
+              height={400}
+              style={{ width: "100%" }}
+              className="w-auto h-auto"
+            />
+          </div>
+          <div className="w-full h-auto text-white">
+            <p className="text-2xl">{post.title}</p>
+            <DateFormatter dateString={post.date} />
+            <div
+              className={markdownStyles["markdown"]}
+              dangerouslySetInnerHTML={{ __html: content }}
+            />
+          </div>
         </div>
+        
       </main>
     </div>
   );
---
+++ app/posts/layout.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import '../../styles/globals.css'
+import Footer from '../components/footer'
+import Navbar from '../components/navbar'
+import Image from 'next/image'
+import DateFormatter from '../../components/DateFormatter'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <main>
+        <div>
+            {/* <head>
+            <title>Blog - Karthik Shankar</title>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-blog-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-blog-16x16.png" />
+            </head> causes hydration error */} 
+            {children}
+        </div>
+    </main>
+      
+  )
+}
