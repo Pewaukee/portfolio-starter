417fa7895d7a43d6c143cf6b649c666240125106
Karthik Shankar <misterkar6@gmail.com>
2023-07-28 13:10:40 -0700

    text shadow style file

---
+++ data/TextShadow.ts
@@ -0,0 +1,6 @@
+import { CSSProperties } from 'react';
+
+export const textShadow: CSSProperties = {
+  textShadow:
+    '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000, 2px 2px 4px rgba(0, 0, 0, 0.5)',
+};
