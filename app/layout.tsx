3999ced2ac25162ce867ee78d849632f0fbfc0f3
Karthik Shankar <misterkar6@gmail.com>
2023-07-28 15:31:20 -0700

    font production attempt #3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,15 @@ import '../styles/globals.css';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 
+// import space mono font
+import { Space_Mono } from 'next/font/google';
+
+const spaceMono = Space_Mono({
+  subsets: ['latin'],
+  weight: '700',
+  style: 'italic',
+});
+
 type Props = {
   children: React.ReactNode;
 };
@@ -15,10 +24,12 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body>
-        {/** all pages have the navbar, main content, and footer */}
-        <Navbar />
-        {children}
-        <Footer />
+        <main className={spaceMono.className}>
+          {/** all pages have the navbar, main content, and footer */}
+          <Navbar />
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   );
