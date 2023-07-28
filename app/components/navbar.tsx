d91a2efbc8ea0226eeb52c34be2ca694b3f2fc55
Karthik Shankar <misterkar6@gmail.com>
2023-07-28 15:09:05 -0700

    formatting

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,30 +1,42 @@
 // create the navbar component
-import Link from "next/link";
-import styles from "../../styles/navbar.module.css";
-import TypingEffect from "./TextAnimation";
-import ThreeComponent from "./ThreeComponent";
+import Link from 'next/link';
+import styles from '../../styles/navbar.module.css';
+import TypingEffect from './TextAnimation';
+import ThreeComponent from './ThreeComponent';
 
 const Navbar: React.FC = () => {
+  {
+    /** add in the name with animation */
+  }
+  const left = <TypingEffect text={'> Karthik Shankar'} speed={0.08} />;
 
-    {/** add in the name with animation */}
-    const left = <TypingEffect text={"> Karthik Shankar"} speed={0.08}/>   
+  {
+    /** add in the center logo */
+  }
+  const center = <div>KS</div>;
 
-    {/** add in the center logo */}
-    const center = <div>KS</div>
-    
-    {/** add in the links */}
-    const right = <ul>
-                    <Link href="/">Home</Link>
-                    <Link href="/portfolio">Portfolio</Link>
-                    <Link href="/posts">Posts</Link>
-                  </ul>
+  {
+    /** add in the links */
+  }
+  const right = (
+    <ul>
+      <Link href="/">Home</Link>
+      <Link href="/portfolio">Portfolio</Link>
+      <Link href="/posts">Posts</Link>
+    </ul>
+  );
 
-    {/* pass into component prop with custom styles */}
-    return (
-        <ThreeComponent left={left} center={<Link href="/">
-{center}
-        </Link>} right={right} styles={styles} />
-    )
-}
+  {
+    /* pass into component prop with custom styles */
+  }
+  return (
+    <ThreeComponent
+      left={left}
+      center={<Link href="/">{center}</Link>}
+      right={right}
+      styles={styles}
+    />
+  );
+};
 
 export default Navbar;
