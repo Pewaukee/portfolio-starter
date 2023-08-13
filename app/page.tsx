5223abf38b5305efa8c82c96cdf671d4c7ad901b
Karthik Shankar <misterkar6@gmail.com>
2023-08-13 16:30:52 -0700

    refactor/data abstraction

--- app/page.tsx
+++ app/page.tsx
@@ -1,189 +1,79 @@
 import '../styles/globals.css';
-import Mapper from './components/Mapper';
 import { textShadow } from '../data/TextShadow';
+import FadeUp from './components/FadeUp';
+import Description from './components/Description';
+import Divider from './components/Divider';
+import Image from 'next/image';
+import { homeData } from '../data/homeData';
 
 const Home: React.FC = () => {
-
   const goldStyledText =
     'flex justify-center items-center p-10 bg-gradient-to-br from-gray-400 to-gray-800 text-3xl';
 
-  const styles = {
-    main: 'relative flex flex-col md:flex-row justify-between p-3 sm:p-4 md:p-5 lg:p-6 xl:p-7 min-h-[500px] w-full fade-in-content duration-75 ease-in-out bg-gradient-radial from-gray-700 to-gray-800',
-    leftContainer: 'relative w-full md:w-[50%] h-full pr-[2%] mb-[40px] md:mb-0',
-    centerContainer:
-      'w-full md:w-[1px] h-[1px] md:h-full bg-white left-[50%] flex justify-center items-center',
-    rightContainer:
-      'w-full md:w-[50%] h-full pl-[5%] flex flex-col jsutify-center items-center mt-[20px] md:mt-0',
+  const renderBreaks = (index: number) => {
+    // if at the end of intro section, render the About Me section header
+    if (index === 0) {
+      return (
+        <div className={goldStyledText}>
+          <h1 className="text-yellow-400" style={textShadow}>
+            <i>About Me!</i>
+          </h1>
+        </div>
+      );
+    }
+    // if at the end of about me section, render the events header
+    else if (index === 2) {
+      return (
+        <div className={goldStyledText}>
+          <h1 className="text-yellow-400" style={textShadow}>
+            <i>Events!</i>
+          </h1>
+        </div>
+      );
+    }
+    // otherwise, render a break
+    else {
+      return <Divider />;
+    }
   };
 
-  const eventsData = [
-    {
-      id: 1,
-      containerStyle: 'w-[50%] h-[50%] rounded-[25px]',
-      mainImage: {
-        className: 'border-4 border-solid border-gray-400 rounded-2xl h-full w-full',
-        src: '/images/ccdc.png',
-        alt: 'ccdc',
-        width: 381,
-        height: 470,
-      },
-      icons: [
-        {
-          src: '/svg/hacker/hacker.svg',
-          alt: 'hacker',
-          width: 100,
-          height: 100,
-        },
-        {
-          src: '/svg/hacker/defense.svg',
-          alt: 'defense',
-          width: 100,
-          height: 100,
-        },
-      ],
-      descriptionText: {
-        title: ['$ cd ~/events', '$ cat ccdc.txt'],
-        text: `I competed in the 2023 Northeast Collegiate Cyber Defense Competition (NECCDC) \n
-                with the UMass Amherst Cybersecurity Club, where we got third place! I was a member \n
-                of the Blue Team, which was tasked with defending a network from a Red Team of hackers. \n
-                In specific, I worked on Linux hardening, writing various incident responses \n
-                and securing the network. `,
-      },
-    },
-  ];
-  
-  const introData = [
-    {
-      id: 1,
-      containerStyle: 'w-[50%] h-[50%]',
-      mainImage: {
-        className:
-          'border-4 border-solid border-gray-400 rounded-2xl w-full h-full',
-        src: '/images/avatar.jpeg',
-        alt: 'avatar',
-        width: 400,
-        height: 400,
-      },
-      icons: [
-        {
-          src: '/svg/hobbies/code.svg',
-          alt: 'code',
-          width: 100,
-          height: 100,
-        },
-        {
-          src: '/svg/hobbies/book.svg',
-          alt: 'book',
-          width: 100,
-          height: 100,
-        },
-      ],
-      descriptionText: {
-        title: ['$ whoami'],
-        text: `Hello! My name is Karthik Shankar. \n
-          I am currently a student at the \n
-          University of Massachusetts Amherst \n
-          studying Computer Science. In my free time, \n
-          I like to read, listen to music, and improve \n
-          my coding skills, like you're seeing here in \n
-          this website. In CS, I am particularly interested \n
-          in website development, cybersecurity, and machine \n
-          learning. Thank you for checking out my work!`,
-      },
-    },
-  ];
-  
-  const aboutMeData = [
-    {
-      id: 1,
-      containerStyle: 'w-[50%] h-[50%] rounded-[25px]',
-      mainImage: {
-        className: 'border-4 border-solid border-gray-400 rounded-2xl w-full h-full',
-        src: '/images/snowboarding.jpeg',
-        alt: 'snowboarding',
-        width: 350,
-        height: 350,
-      },
-      icons: [
-        {
-          src: '/svg/hobbies/snowboard.svg',
-          alt: 'snowboard',
-          width: 100,
-          height: 100,
-        },
-        {
-          src: '/svg/hobbies/snow.svg',
-          alt: 'snow',
-          width: 100,
-          height: 100,
-        },
-      ],
-      descriptionText: {
-        title: ['$ cd ~/hobbies', '$ cat snowboarding.txt'],
-        text: `One of my favorite hobbies is snowboarding. I have been snowboarding for \n
-              over 10 years now, and I have been to many mountains in the Western US. \n
-              I've been to California, Nevada, Utah, Colorado, Wyoming, and Montana resorts \n
-              for some powder. My personal favorite is Big Sky, Montana.`,
-      },
-    },
-    {
-      id: 2,
-      containerStyle: 'w-[50%] h-[50%] rounded-[20px]',
-      mainImage: {
-        className: 'border-4 border-solid border-gray-400 rounded-2xl w-full h-full',
-        src: '/images/chess.gif',
-        alt: 'chess',
-        width: 400,
-        height: 400,
-      },
-      icons: [
-        {
-          src: '/svg/hobbies/chess/chess_clock.svg',
-          alt: 'chess',
-          width: 100,
-          height: 100,
-        },
-        {
-          src: '/svg/hobbies/chess/king.svg',
-          alt: 'king',
-          width: 100,
-          height: 100,
-        },
-      ],
-      descriptionText: {
-        title: ['$ cd ~/hobbies', '$ cat chess.txt'],
-        text: `I have been playing chess consistently for a little \n
-              over a year now. I am currently rated around 1000 on chess.com! \n
-              Here is a gif of a recent game I played. I had a huge attack here, \n
-              usually my games don't go this well...`,
-      },
-    },
-  ];
-  
-
   return (
-    // pass into component prop with custom styles
     <>
-      {/** intro */}
-      <Mapper array={introData} styles={styles} type="picture" />
-
-      <div className={goldStyledText}>
-        <h1 className="text-yellow-400" style={textShadow}>
-          <i>About Me!</i>
-        </h1>
-      </div>
-
-      {/** about me */}
-      <Mapper array={aboutMeData} styles={styles} type="picture" />
-
-      <div className={goldStyledText}>
-        <h1 className="text-yellow-400" style={textShadow}>
-          <i>Events</i>
-        </h1>
-      </div>
-
-      {/** events */}
-      <Mapper array={eventsData} styles={styles} type="picture" />
+      {homeData.map((data, index) => (
+        <main key={index}>
+          <FadeUp
+            className="relative flex flex-col md:flex-row justify-between
+           p-3 sm:p-4 md:p-5 lg:p-6 xl:p-7 
+           min-h-[500px] w-full fade-in-content
+            duration-75 ease-in-out 
+            bg-gradient-radial from-gray-700 to-gray-800"
+          >
+            <div className="relative w-full h-auto md:w-[50%] mb-[40px] md:mb-0">
+              <div className="flex w-full h-auto justify-center items-center">
+                <Image
+                  src={data.mainImage.src}
+                  alt={data.mainImage.alt}
+                  width={data.mainImage.width}
+                  height={data.mainImage.height}
+                  className="rounded-2xl border-4 border-solid border-gray-400"
+                />
+              </div>
+            </div>
+            <div
+              className="w-full md:w-[1px] h-[1px] md:h-auto
+                 bg-white left-[50%] flex justify-center items-center"
+            />
+            <div
+              className="w-full md:w-[50%] h-full pl-[5%] flex flex-col 
+                 justify-center items-center mt-[20px] md:mt-0"
+            >
+              <Description title={data.title} text={data.text} />
+            </div>
+          </FadeUp>
+          {/** if at last index, don't call this function */}
+          {index !== homeData.length - 1 ? renderBreaks(index) : null}
+        </main>
+      ))}
     </>
   );
 };
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,145 +4,36 @@ import Divider from '../components/Divider';
 import FadeUp from '../components/FadeUp';
 import Icon from '../components/Icon';
 import Video from './components/Video';
+import { portfolioData } from '../../data/portfolioData';
 
 const Portfolio: React.FC = () => {
-  const styles = {
-    main: 'relative flex flex-col md:flex-row justify-between p-2 md:min-h-[400px] w-full fade-in-content duration-75 ease-in-out bg-gradient-radial from-gray-700 to-gray-800',
-    leftContainer:
-      'relative w-full md:w-[50%] h-full flex justify-center items-center mb-[40px] md:mb-0',
-    centerContainer:
-      'w-full md:w-[1px] h-[1px] md:h-full bg-white left-[50%] flex justify-center items-center',
-    rightContainer:
-      'w-full md:w-[50%] h-full pl-[5%] mt-[25px] md:mt-0 flex flex-col justify-center items-center',
-  };
-
-  const portfolioData = [
-    {
-      id: 1,
-      containerStyle:
-        'flex w-[75%] h-[75%] justify-center items-center shadow-lg',
-      mainImage: {
-        className: 'border-4 rounded-2xl',
-        src: '/projects/website.mp4',
-        alt: 'project website video',
-        width: 1000,
-        height: 1000,
-      },
-      icons: [
-        {
-          src: '/svg/portfolio/webdev_1.svg',
-          alt: 'webdev icon',
-          width: 70,
-          height: 70,
-        },
-        {
-          src: '/svg/portfolio/webdev_2.svg',
-          alt: 'webdev icon',
-          width: 70,
-          height: 70,
-        },
-      ],
-      descriptionText: {
-        title: ['Personal Website Project'],
-        text: "In this project, I have learned and am continuing to learn much more about website development using the Next JS framework. I have also learned how to work with complex CSS styling and animations like framer motion and keyframe animations, all essential to making a website look more than simply a static page. It has been a fun journey so far, here's to more web dev!",
-      },
-    },
-    {
-      id: 2,
-      containerStyle:
-        'flex w-[85%] h-[85%] justify-center items-center shadow-lg',
-      mainImage: {
-        className: 'border-4 rounded-2xl',
-        src: '/projects/modern_shopper.mp4',
-        alt: 'modern shopper project video',
-        width: 1000,
-        height: 1000,
-      },
-      icons: [
-        {
-          src: '/svg/portfolio/shopping_basket.svg',
-          alt: 'shopping basket icon',
-          width: 70,
-          height: 70,
-        },
-        {
-          src: '/svg/portfolio/online_shopping.svg',
-          alt: 'online shopping icon',
-          width: 70,
-          height: 70,
-        },
-      ],
-      descriptionText: {
-        title: ['Modern Shopper'],
-        text: "I used selenium to create an automated shopping experience from a command line interface. Using the Selenium WebDriver, I was able to put in a prompt for an item and get the most relevant results from Amazon's database of millions of products. This project could be used as an API or as a way to more efficiently shop online. With Chrome being automatically controlled, so much more is possible!",
-      },
-    },
-    {
-      id: 3,
-      containerStyle:
-        'flex w-[85%] h-[85%] justify-center items-center shadow-lg',
-      mainImage: {
-        className: 'border-4 rounded-2xl',
-        src: '/projects/spotify.mp4',
-        alt: 'spotify project video',
-        width: 1000,
-        height: 1000,
-      },
-      icons: [
-        {
-          src: '/svg/portfolio/music_note.svg',
-          alt: 'spotify icon',
-          width: 70,
-          height: 70,
-        },
-        {
-          src: '/svg/portfolio/guitar.svg',
-          alt: 'music icon',
-          width: 70,
-          height: 70,
-        },
-      ],
-      descriptionText: {
-        title: ['Spotify Clone'],
-        text: `I used the Deezer API to create a clone of 
-        the popular music streaming service Spotify. You can search
-        for music, artists, and albums, and play them in the browser.
-        In this project, I learned a lot about the complexities of
-        state management throughout an application. I also learned about
-        complexities of front-end development, and learned how to use
-        Radix UI Components as well as my own custom elements to make a
-        unique UI experience for the user to enjoy.`,
-      },
-    },
-  ];
-
   return (
     <>
-      <div className="m-5 flex justify-center items-center m-4">
+      <div className="flex justify-center items-center bg-gray-600 py-8">
         <h1 className="text-2xl">Welcome to my Portfolio!</h1>
       </div>
-      {/* <div className="container m-10 border-2 border-white-600 w-auto p-10">
-                <div className="flex items-center gap-6"> */}
-      {/* <Mapper array={portfolioData} styles={styles} type='video'/> */}
       {portfolioData.map((data, index) => (
-        <div key={index}>
-          <FadeUp className={styles.main}>
-            <div className={styles.leftContainer}>
+        <main key={index}>
+          <FadeUp
+            className="relative flex flex-col md:flex-row justify-between p-2 md:min-h-[400px] w-full 
+          fade-in-content duration-75 ease-in-out bg-gradient-radial from-gray-700 to-gray-800"
+          >
+            <div className="relative w-full md:w-[50%] h-full flex justify-center items-center mb-[40px] md:mb-0">
               <div className="flex w-full flex-col md:flex-row justify-center items-center">
-                <div className="relative w-[90%] h-auto">
+                <div className="relative mt-4 md:mt-8 w-[90%] h-auto">
                   <Icon icons={data.icons} />
-                  <Video videoProps={data.mainImage} />
+                  <Video src={data.src} />
                 </div>
               </div>
             </div>
 
-            <div className={styles.centerContainer}></div>
-            <div className={styles.rightContainer}>
-              <Description descriptionText={data.descriptionText} />
+            <div className="w-full md:w-[1px] h-[1px] md:h-full bg-white left-[50%] flex justify-center items-center"></div>
+            <div className="w-full md:w-[50%] h-full pl-[5%] mt-[25px] md:mt-0 flex flex-col justify-center items-center">
+              <Description title={data.title} text={data.text} />
             </div>
           </FadeUp>
           {index !== portfolioData.length - 1 ? <Divider /> : null}
-        </div>
+        </main>
       ))}
     </>
   );
