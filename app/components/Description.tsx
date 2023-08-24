d397713afb1c5690fa015b23a033a74fcd4d6fb4
Karthik Shankar <misterkar6@gmail.com>
2023-08-23 18:49:27 -0700

    change argument type for title

--- app/components/Description.tsx
+++ app/components/Description.tsx
@@ -1,21 +1,34 @@
 // rendering the description text
-import { textShadow } from "../../data/TextShadow";
-import TypingEffect from "./TextAnimation";
+import { textShadow } from '../../data/TextShadow';
+import TypingEffect from './TextAnimation';
 
-function Description({title, text}: {title: string[], text: string}) {
-    return ( 
-        <div style={textShadow} className="text-white">
-            {title.map((t) => {
-                return (
-                    <div key={t}>
-                        <h1>{t}</h1>
-                        <br/>
-                    </div>
-                )
-            })}
-            <TypingEffect text={text} speed={0.02}/>
-        </div>
-    );
+function Description({
+  title,
+  text,
+}: {
+  title: string[] | string;
+  text: string;
+}) {
+  return (
+    <div style={textShadow} className="text-white">
+      {typeof title === 'string' ? (
+        <>
+          <h1>{title}</h1>
+          <br />
+        </>
+      ) : (
+        title.map((t) => {
+          return (
+            <div key={t}>
+              <h1>{t}</h1>
+              <br />
+            </div>
+          );
+        })
+      )}
+      <TypingEffect text={text} speed={0.02} />
+    </div>
+  );
 }
 
 export default Description;
