379b004655c0242d6633dc436c9e67cdd8868639
Karthik Shankar <misterkar6@gmail.com>
2023-08-28 16:15:34 -0700

    blog page stylistic change

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,13 +1,21 @@
-import { getPostBySlug } from "../../../lib/api";
-import markdownToHtml from "../../../lib/markdownToHtml";
-import markdownStyles from "./markdown-styles.module.css";
-import DateFormatter from "../../../components/DateFormatter";
-import Image from "next/image";
+import { getPostBySlug } from '../../../lib/api';
+import markdownToHtml from '../../../lib/markdownToHtml';
+import markdownStyles from './markdown-styles.module.css';
+import DateFormatter from '../../../components/DateFormatter';
+import Image from 'next/image';
+import Link from 'next/link';
+import { ArrowBigLeftDash } from 'lucide-react';
 
 export default async function Post({ params }: { params: { slug: string } }) {
-  const post = getPostBySlug(params.slug, ["title", "author", "content", "date", "coverImage"]);
+  const post = getPostBySlug(params.slug, [
+    'title',
+    'author',
+    'content',
+    'date',
+    'coverImage',
+  ]);
 
-  const content = await markdownToHtml(post.content || "");
+  const content = await markdownToHtml(post.content || '');
 
   return (
     <div className="container mx-auto">
@@ -19,20 +27,28 @@ export default async function Post({ params }: { params: { slug: string } }) {
               alt={`cover image for ${post.title}`}
               width={400}
               height={400}
-              style={{ width: "100%" }}
+              style={{ width: '100%' }}
               className="w-auto h-auto"
             />
           </div>
+
           <div className="w-full h-auto text-white">
             <p className="text-2xl">{post.title}</p>
             <DateFormatter dateString={post.date} />
             <div
-              className={markdownStyles["markdown"]}
+              className={markdownStyles['markdown']}
               dangerouslySetInnerHTML={{ __html: content }}
             />
           </div>
         </div>
-        
+        <Link href="/posts">
+          <div className="inline-flex group/back">
+            <div className="group-hover/back:-translate-x-1 duration-100">
+              <ArrowBigLeftDash />
+            </div>
+            <h1 className="ml-1">Back to Main</h1>
+          </div>
+        </Link>
       </main>
     </div>
   );
